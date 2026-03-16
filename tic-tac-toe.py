60824d3a1d719fb3ef56a37b1000f03cb36f613d
Dalete Ferreira <daleteferreirac@gmail.com>
2026-03-15 21:30:20 -0300

    add recursion

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,24 +2,6 @@ from random import randrange
 
 board = [[1, 2, 3], [4, "X", 6], [7, 8, 9]]
 
-def showBoard(board):
-    print("+--+--+--+")
-    for i in range(3):
-        print(board[i])
-    print("+--+--+--+")
-
-showBoard(board)
-
-def enterMove(board):
-    move = int(input("whats your move? "))
-    for l, c in enumerate(board):  # l-> line, c-> item
-        for i, j in enumerate(c):  # i-> index, j-> value, [1, 2, 3]
-            if j == move:
-                board[l][i] = "O"
-    return showBoard(board)
-
-enterMove(board)
-
 def list_of_free_spaces(board):
     freeSpaces = []
     for l, c in enumerate(board):
@@ -30,7 +12,27 @@ def list_of_free_spaces(board):
                 freeSpaces.append(j)
     return freeSpaces
 
-print(list_of_free_spaces(board))
+def showBoard(board):
+    print("+--+--+--+")
+    for i in range(3):
+        print(board[i])
+    print("+--+--+--+")
+
+showBoard(board)
+
+def enterMove(board):
+    move = int(input("whats your move? "))
+    if move not in list_of_free_spaces(board):
+        print("invalid move, try again")
+        return enterMove(board)
+    else:
+        for l, c in enumerate(board):  # l-> line, c-> item
+            for i, j in enumerate(c):  # i-> index, j-> value, [1, 2, 3]
+                if j == move:
+                    board[l][i] = "O"
+    return showBoard(board)
+
+enterMove(board)
 
 
 
