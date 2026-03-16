842d03451ba6c68d4c4274ef1d01a5487bacf24e
Dalete Ferreira <daleteferreirac@gmail.com>
2026-03-16 10:08:31 -0300

    fix

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,16 +10,6 @@ def showBoard(board):
 
 showBoard(board)
 
-def enterMove(board):
-    move = int(input("whats your move? "))
-    for l, c in enumerate(board):  # l-> line, c-> item
-        for i, j in enumerate(c):  # i-> index, j-> value, [1, 2, 3]
-            if j == move:
-                board[l][i] = "O"
-    return showBoard(board)
-
-enterMove(board)
-
 def list_of_free_spaces(board):
     freeSpaces = []
     for l, c in enumerate(board):
@@ -30,7 +20,22 @@ def list_of_free_spaces(board):
                 freeSpaces.append(j)
     return freeSpaces
 
-print(list_of_free_spaces(board))
+def enterMove(board):
+    move = int(input("whats your move? "))
+    if move not in list_of_free_spaces(board):
+        print("invalid move, try again")
+        return enterMove(board)
+    else:
+        for l, c in enumerate(board):  # l-> line, c-> item
+            for i, j in enumerate(c):  # i-> index, j-> value, [1, 2, 3]
+                if j == move:
+                    board[l][i] = "O"
+    return showBoard(board)
+
+enterMove(board)
+
+
+
 
 
 
